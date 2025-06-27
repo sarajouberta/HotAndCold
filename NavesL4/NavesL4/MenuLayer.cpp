f23f110f917da8347e558daf006f4dc885efbb83
Sara Álvarez Dos Santos <UO226305@uniovi.es>
2025-06-27 18:45:00 +0200

    Cambiada la imagen inicial del juego.

--- NavesL4/NavesL4/MenuLayer.cpp
+++ NavesL4/NavesL4/MenuLayer.cpp
@@ -8,7 +8,7 @@ MenuLayer::MenuLayer(Game* game)
 
 void MenuLayer::init() {
 	// Fondo normal, sin velocidad
-	background = new Background("res/menu_fondo.png", WIDTH * 0.5, HEIGHT * 0.5, game);
+	background = new Background("res/hotNcold_fondo.png", WIDTH * 0.5, HEIGHT * 0.5, game);
 	//mín: 1 botón:
 	button = new Actor("res/boton_jugar.png", WIDTH * 0.5, HEIGHT * 0.7, 232, 72, game);
 }
