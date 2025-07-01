ca6544a81650c1eb2c11eef1fa74cb406030b8a8
Sara Álvarez Dos Santos <UO226305@uniovi.es>
2025-07-01 20:16:36 +0200

    Corrijo imagen errónea de la FinishedLevelLayer.

--- NavesL4/NavesL4/FinishedLevelLayer.cpp
+++ NavesL4/NavesL4/FinishedLevelLayer.cpp
@@ -7,7 +7,7 @@ FinishedLevelLayer::FinishedLevelLayer(Game* game) : Layer(game) {
 }
 
 void FinishedLevelLayer::init() {
-    background = new Actor("res/victory_menu1.png", WIDTH * 0.5, HEIGHT * 0.5, WIDTH, HEIGHT, game);
+    background = new Actor("res/finished_level_menu.png", WIDTH * 0.5, HEIGHT * 0.5, WIDTH, HEIGHT, game);
     buttonNext = new Actor("res/boton_siguiente_nivel.png", WIDTH * 0.35, HEIGHT * 0.6, 232, 72, game);
     buttonExit = new Actor("res/boton_salir.png", WIDTH * 0.65, HEIGHT * 0.6, 232, 72, game);
 }
